d43470d5d0600ca4b82c1c59e0fbbb944ba2f60f
1mick1mfn-creator <1mick1mfn-creator@users.noreply.github.com>
2026-02-24 22:01:02 -0500

    Initial Astro commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://1mick1mfn-creator.github.io',
+  base: '/astro-allo-toi',
+});
